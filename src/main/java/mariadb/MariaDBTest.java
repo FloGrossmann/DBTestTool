5dc83ce9ab9e484520f07cd752b5ee327bf2702d
Kevin Hink <hink.kevin@dh-karlsruhe.de>
2020-09-28 16:04:57 +0200

    added variables

--- src/main/java/mariadb/MariaDBTest.java
+++ src/main/java/mariadb/MariaDBTest.java
@@ -1,5 +1,14 @@
 package mariadb;
 
+import org.mariadb.jdbc.MariaDbConnection;
+
 public class MariaDBTest {
+	
+	String connectionString;
+	MariaDbConnection mariaDbConnection;
+	
+	public MariaDBTest() {
+		// TODO Auto-generated constructor stub
+	}
 
 }

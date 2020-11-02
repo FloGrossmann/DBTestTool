cfd7411eb6a2a95968d194b57b73417090014290
Florian <florian4774@gmail.com>
2020-11-02 10:09:59 +0100

    updtaed accessTime to store the averaged results of multiple tests

--- src/main/java/measure/AccessTime.java
+++ src/main/java/measure/AccessTime.java
@@ -2,28 +2,50 @@ package measure;
 
 import com.opencsv.bean.CsvBindByName;
 
-public class AccessTime extends CsvBean{
+public class AccessTime extends CsvBean {
 
-	
 	@CsvBindByName(column = "Kategorie", required = true)
 	CRUDoperation kategorie;
-	
+
 	@CsvBindByName(column = "Objekt", required = true)
 	ObjectCategory objektKategorie;
-	
+
 	@CsvBindByName(column = "Methode", required = true)
 	MethodType methode;
-	
-	@CsvBindByName(column = "Zeit in Nanosekunden", required = true)
-	long zugriffszeitZeit;
-	
-	public AccessTime(CRUDoperation crudCategory, ObjectCategory objectCategory, long time, MethodType methodType) {
+
+	@CsvBindByName(column = "Durchschnitts Zeit in Nanosekunden", required = true)
+	double durchschnittsZugriffsZeit;
+
+	@CsvBindByName(column = "Varianz der Messpunkte", required = true)
+	double varianz;
+
+	@CsvBindByName(column = "Standardabweichung der Messpunkte", required = true)
+	double standardAbweichung;
+
+	@CsvBindByName(column = "Anzahl der durchgeführten Messungen", required = true)
+	double averaged_over;
+
+	@CsvBindByName(column = "Anzahl der sich in der Datenbank befindlichen Einträge", required = true)
+	int testedAt;
+
+	public AccessTime(CRUDoperation crudCategory, ObjectCategory objectCategory, MethodType methodType,
+			double average_time, double varianz, double standardAbweichung, int averaged_over, int testedAt) {
 		this.methode = methodType;
-		this.zugriffszeitZeit = time;
-		this.kategorie=crudCategory;
+		this.durchschnittsZugriffsZeit = average_time;
+		this.kategorie = crudCategory;
 		this.objektKategorie = objectCategory;
+		this.varianz = varianz;
+		this.standardAbweichung = standardAbweichung;
+		this.averaged_over = averaged_over;
+		this.testedAt = testedAt;
 	}
-	
+
+	public AccessTime(CRUDoperation curdCategory, ObjectCategory objectCategory, MethodType methodType,
+			Messreihe messreihe, int testedAt) {
+		this(curdCategory, objectCategory, methodType, messreihe.getDurchschnitt(), messreihe.getVarianz(),
+				messreihe.getStandardAbweichung(), messreihe.getMessungen().size(), testedAt);
+	}
+
 	public ObjectCategory getObjektKategorie() {
 		return objektKategorie;
 	}
@@ -40,12 +62,12 @@ public class AccessTime extends CsvBean{
 		this.methode = methode;
 	}
 
-	public long getZugriffszeitZeit() {
-		return zugriffszeitZeit;
+	public double getZugriffszeitZeit() {
+		return durchschnittsZugriffsZeit;
 	}
 
 	public void setZugriffszeitZeit(long zugriffszeitZeit) {
-		this.zugriffszeitZeit = zugriffszeitZeit;
+		this.durchschnittsZugriffsZeit = zugriffszeitZeit;
 	}
 
 	public CRUDoperation getKategorie() {
@@ -55,7 +77,45 @@ public class AccessTime extends CsvBean{
 	public void setKategorie(CRUDoperation kategorie) {
 		this.kategorie = kategorie;
 	}
-	
-	 
-	
+
+	public double getDurchschnittsZugriffsZeit() {
+		return durchschnittsZugriffsZeit;
+	}
+
+	public void setDurchschnittsZugriffsZeit(double durchschnittsZugriffsZeit) {
+		this.durchschnittsZugriffsZeit = durchschnittsZugriffsZeit;
+	}
+
+	public double getVarianz() {
+		return varianz;
+	}
+
+	public void setVarianz(double varianz) {
+		this.varianz = varianz;
+	}
+
+	public double getStandardAbweichung() {
+		return standardAbweichung;
+	}
+
+	public void setStandardAbweichung(double standardAbweichung) {
+		this.standardAbweichung = standardAbweichung;
+	}
+
+	public double getAveraged_over() {
+		return averaged_over;
+	}
+
+	public void setAveraged_over(double averaged_over) {
+		this.averaged_over = averaged_over;
+	}
+
+	public int getTestedAt() {
+		return testedAt;
+	}
+
+	public void setTestedAt(int testedAt) {
+		this.testedAt = testedAt;
+	}
+
 }
